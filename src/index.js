fbdd281ba4ac6a63a86a314cb2b0322ac6c2451a
Khusainov779 <khusainov779@gmail.com>
2020-03-10 19:19:02 +0500

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    if (n < 0) {
+        n = -n;
+    }
+    let reversedNum = n
+        .toString()
+        .split("")
+        .reverse()
+        .join("");
+    return reversedNum;
+};
